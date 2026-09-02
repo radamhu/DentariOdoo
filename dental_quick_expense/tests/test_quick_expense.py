5ce788cfa8478eee1825ca10e7558575962e1b3a
Ferko <ferko@ferkos-air.lan>
2026-09-02 20:05:02 +0200

    feat: dental.quick.expense wizard creates draft Vendor Bill

--- dental_quick_expense/tests/test_quick_expense.py
+++ dental_quick_expense/tests/test_quick_expense.py
@@ -1,3 +1,4 @@
+from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase, tagged
 
 
@@ -11,3 +12,59 @@ class TestQuickExpenseCategories(TransactionCase):
         accounts = quick_expense_category_accounts(self.env)
         self.assertEqual(len(accounts), 11)
         self.assertTrue(all(a.account_type == 'expense' for a in accounts))
+
+
+@tagged('post_install', '-at_install')
+class TestQuickExpenseWizard(TransactionCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.partner = cls.env['res.partner'].create({'name': 'Teszt Szállító'})
+        from odoo.addons.dental_quick_expense.models.account_move import (
+            quick_expense_category_accounts,
+        )
+        cls.category = quick_expense_category_accounts(cls.env)[0]
+        cls.tax = cls.env['account.tax'].search(
+            [('type_tax_use', '=', 'purchase')], limit=1,
+        )
+
+    def test_action_save_creates_draft_vendor_bill(self):
+        wizard = self.env['dental.quick.expense'].create({
+            'date': '2026-09-02',
+            'partner_id': self.partner.id,
+            'category_account_id': self.category.id,
+            'description': 'Teszt kiadás',
+            'net_amount': 5000,
+            'tax_id': self.tax.id,
+        })
+        wizard.action_save()
+
+        moves = self.env['account.move'].search([
+            ('partner_id', '=', self.partner.id),
+            ('move_type', '=', 'in_invoice'),
+        ])
+        self.assertEqual(len(moves), 1)
+        move = moves[0]
+        self.assertEqual(move.state, 'draft')
+        self.assertEqual(len(move.invoice_line_ids), 1)
+        line = move.invoice_line_ids[0]
+        self.assertEqual(line.account_id, self.category)
+        self.assertEqual(line.name, 'Teszt kiadás')
+        self.assertEqual(line.price_unit, 5000)
+
+    def test_missing_required_field_blocks_save(self):
+        with self.assertRaises(ValidationError):
+            self.env['dental.quick.expense'].create({
+                'partner_id': self.partner.id,
+                'category_account_id': self.category.id,
+                'description': 'Teszt hiányos',
+                'tax_id': self.tax.id,
+                # net_amount intentionally omitted
+            })
+        moves = self.env['account.move'].search([
+            ('partner_id', '=', self.partner.id),
+            ('ref', '=', False),
+            ('invoice_line_ids.name', '=', 'Teszt hiányos'),
+        ])
+        self.assertEqual(len(moves), 0)
---
+++ dental_quick_expense/wizard/quick_expense.py
@@ -0,0 +1,73 @@
+from odoo import _, fields, models
+from odoo.exceptions import UserError
+
+from ..models.account_move import quick_expense_category_accounts
+
+
+class DentalQuickExpense(models.TransientModel):
+    _name = 'dental.quick.expense'
+    _description = 'Gyors kiadás rögzítése'
+
+    date = fields.Date(
+        string='Dátum', required=True, default=fields.Date.context_today,
+    )
+    partner_id = fields.Many2one(
+        'res.partner', string='Szállító', required=True,
+    )
+    category_account_id = fields.Many2one(
+        'account.account',
+        string='Kategória',
+        required=True,
+        domain=lambda self: [('id', 'in', quick_expense_category_accounts(self.env).ids)],
+    )
+    description = fields.Char(string='Leírás', required=True)
+    currency_id = fields.Many2one(
+        'res.currency',
+        default=lambda self: self.env.company.currency_id,
+        required=True,
+    )
+    net_amount = fields.Monetary(
+        string='Nettó összeg', required=True, currency_field='currency_id',
+    )
+    tax_id = fields.Many2one(
+        'account.tax',
+        string='ÁFA',
+        required=True,
+        domain=[('type_tax_use', '=', 'purchase')],
+    )
+    ref = fields.Char(string='Bizonylatszám')
+    attachment_ids = fields.Many2many('ir.attachment', string='Bizonylat')
+
+    def action_save(self):
+        self.ensure_one()
+        if not self.category_account_id.exists():
+            raise UserError(_('A kiadás kategória nem található.'))
+
+        move = self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'partner_id': self.partner_id.id,
+            'invoice_date': self.date,
+            'ref': self.ref,
+            'invoice_line_ids': [(0, 0, {
+                'account_id': self.category_account_id.id,
+                'name': self.description,
+                'quantity': 1,
+                'price_unit': self.net_amount,
+                'tax_ids': [(6, 0, [self.tax_id.id])],
+            })],
+        })
+
+        if self.attachment_ids:
+            self.attachment_ids.write({
+                'res_model': 'account.move',
+                'res_id': move.id,
+            })
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Kiadás'),
+            'res_model': 'account.move',
+            'view_mode': 'form',
+            'res_id': move.id,
+            'target': 'current',
+        }
